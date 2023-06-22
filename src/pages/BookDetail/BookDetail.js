b9f55e9d7f803015f4c3152d82c56a8d8f76d1fc
Cuong Pham <pvcuongkg123@gmail.com>
2023-06-22 16:51:19 +0700

    commit

--- src/pages/BookDetail/BookDetail.js
+++ src/pages/BookDetail/BookDetail.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
-import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link, Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
 
 const BookDetail = (props) => {
     const navigate = useNavigate();
-    const [book, setBook] = useState({});
+    const [book, setBook] = useState({ image: { link: '' } });
     const { state } = useLocation();
     const { bookId } = useParams();
 
@@ -31,11 +31,45 @@ const BookDetail = (props) => {
 
     return (
         <Container>
-            {/* {console.log(state)} */}
-            {console.log(book)}
-            <h1>Book Detail</h1>
-            <h3>Name</h3>
-            <h4>{book.name}</h4>
+            <h1 className="text-center my-5">Book Detail</h1>
+            <Row>
+                <Col md={3}>
+                    <img src={book.image.link} alt="" style={{ width: 270 }} />
+                </Col>
+                <Col>
+                    <div className="d-flex align-items-center">
+                        <h3>{book.name}</h3>
+                        <h4 className="mx-3 text-secondary">
+                            {book.bookCover} - {book.publishedDate}
+                        </h4>
+                    </div>
+                    <div>
+                        <div className="mb-4">
+                            by <Link>Author</Link>
+                        </div>
+                        <p>
+                            <b>Publisher:</b> {book.publishedBy}
+                        </p>
+                        <p>
+                            <b>Language:</b> {book.language}
+                        </p>
+                        <p>
+                            <b>Total pages:</b> {book.pageNumbers}
+                        </p>
+                        <p>
+                            <b>Short description:</b>
+                            <span className="mt-3">{book.shortDescription}</span>
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+            <hr />
+            <div>
+                <h3>Same author</h3>
+                <Row>
+                    
+                </Row>
+            </div>
         </Container>
     );
 };
